21a5ae8eee7e73bdc75993a84f372fee78b3de83
Rimgaizauskaite <rimgaizauskaite@gmail.com>
2017-07-25 14:33:14 +0300

    pataisyti

--- scrips/main.js
+++ scrips/main.js
@@ -14,28 +14,23 @@ $(".burger-nav-2").on("click",function(){
 $("nav ul").toggleClass("open");
 });  
     
+});$(document).ready(function(){
+    
+$(".burger-nav-3").on("click",function(){
+    
+$(".theend-body nav ul").toggleClass("open");
+});  
+    
 });
 
-//$(document).ready(function() {
-//   function filter(string) {
-//      return string
-//         .replace(/^\//,'')
-//         .replace(/(index|default)\.[a-zA-Z]{3,4}$/,'')
-//         .replace(/\/$/,'')
-//   }
-//   $('a[href*=#]').each(function() {
-//      if (filter(location.pathname) == filter(this.pathname)
-//         && location.hostname == this.hostname
-//	 && this.hash.replace(/#/,'') ) {
-//	    var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-//	    var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-//	    if ($target) {
-//	       var targetOffset = $target.offset().top;
-//	       $(this).click(function() {
-//	          $('html, body').animate({scrollTop: targetOffset}, 400);
-//	          return false;
-//	        });
-//            }
-//         }
-//      });
-//   });
+$(document).ready(function() {
+  
+  var scrollLink = $('.scroll');
+
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+});
